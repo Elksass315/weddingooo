dce97bf8faaa7cb510cfdc91e9d65544706df87c
Elksass315 <95420181+Elksass315@users.noreply.github.com>
2024-11-12 10:42:28 +0200

    Update mohafza.controller.js

--- src/modules/mohafza/mohafza.controller.js
+++ src/modules/mohafza/mohafza.controller.js
@@ -15,8 +15,8 @@ import {
 // Create a new Mohafza
 export const createMohafza = async (req, res) => {
   try {
-      const { error } = createMohafzaSchema.validate(req.body);
-      if (error) return res.status(400).json({ message: error.details[0].message });
+     // const { error } = createMohafzaSchema.validate(req.body);
+//if (error) return res.status(400).json({ message: error.details[0].message });
 
       const newMohafza = new Mohafza(req.body);
       await newMohafza.save();
